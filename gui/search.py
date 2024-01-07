92babaf19673841028e08d531d4d732de4889f14
Gioele Fiorenza <gioele.fiorenza2000@gmail.com>
2024-01-07 19:55:17 +0100

    Added similarity search to gui

--- gui/search.py
+++ gui/search.py
@@ -1,5 +1,8 @@
 from PyQt6 import QtWidgets, uic
-from PyQt6.QtWidgets import QFileDialog
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QFileDialog, QGridLayout, QLabel, QScrollArea, QWidget
+from similarity_search.similarity_search_gui import load_images_for_gui, find_images_from_gui
 
 
 class SearchWindow(QtWidgets.QMainWindow):
@@ -7,7 +10,15 @@ class SearchWindow(QtWidgets.QMainWindow):
         super(SearchWindow, self).__init__()
         uic.loadUi('gui/similarity.ui', self)
         self.button_select = self.findChild(QtWidgets.QPushButton, 'buttonSelect')
-        self.button_select.clicked.connect(self.select_clicked)
+        self.button_select.clicked.connect(self.select_clicked);
+        self.grid = self.findChild(QtWidgets.QScrollArea, 'gridImages');
+        self.query_img = self.findChild(QtWidgets.QLabel, 'queryImg')
+
+        nn, filenames = load_images_for_gui()
+        self.nn_features = nn;
+        self.filenames = filenames;
+        self.handcrafted = None
+
 
     def select_clicked(self):
         dialog = QFileDialog()
@@ -16,3 +27,38 @@ class SearchWindow(QtWidgets.QMainWindow):
         if dialog.exec():
             filename = dialog.selectedFiles()[0]
             print(filename)
+            pixmap = QPixmap(filename)
+            self.query_img.setPixmap(pixmap.scaledToWidth(200))
+            self.query_img.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+            use_intersection = False,
+            use_nn = True
+
+            most_similar_filenames = find_images_from_gui(filename, self.handcrafted, self.nn_features, self.filenames, use_intersection, use_nn)
+            row, col = 0, 0
+            number_limit = 100
+
+            scrollAreaWidget = QWidget()
+            grid_layout = QGridLayout(scrollAreaWidget)
+            for path in most_similar_filenames:
+                if number_limit <= 0:
+                    continue;
+                number_limit -= 1;
+                pixmap = QPixmap(path)
+                label = QLabel(self)
+                label.setPixmap(pixmap.scaledToWidth(200))
+                label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+                grid_layout.addWidget(label, row, col)
+                col += 1;
+
+                if col == 2:
+                    col = 0;
+                    row += 1
+
+            widget = QWidget()
+            self.grid.setWidget(scrollAreaWidget);
+
+            self.setGeometry(100, 100, 800, 600)
+
+            
---
+++ similarity_search/similarity_search_gui.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import cv2 as cv
+import sys
+import os
+import numpy as np
+
+sys.path.append('..')
+
+from keras.applications.efficientnet_v2 import EfficientNetV2B0, preprocess_input
+from similarity_search.neural_similarity_search import find_similar, find_similar_handcrafted
+from handcrafted_extraction import load_all_features
+
+HANDCRAFTED_FEATURES = 'dataset/handcrafted/'
+NN_FEATURES = 'similarity_search/extracted_features/efficient_net_similarity'
+NN_FILENAMES = 'similarity_search/extracted_features/efficient_net_similarity_filenames.csv'
+IMAGE_DIR = 'dataset/complete/'
+
+
+def load_images_for_gui(type='nn'):
+    if type == 'nn':
+        features = np.load(NN_FEATURES + '.npy')
+    else:
+        features, _ = load_all_features(HANDCRAFTED_FEATURES, os.listdir(IMAGE_DIR), load_color=True, load_lbp=True, load_gabor=False, load_sift=False, load_bow=False)
+
+    filenames = pd.read_csv(NN_FILENAMES, header=None).iloc[:, 1].values
+
+    return features, filenames
+
+def find_images_from_gui(query_path, features_handcrafted, features_nn, filenames, use_intersection, use_nn):
+    from feature_extraction import build_feature_extractor
+    img = cv.imread(query_path);
+    img = cv.cvtColor(img, cv.COLOR_BGR2RGB);
+
+    image_limit = 1000
+    nn_most_similar, distances = find_similar(build_feature_extractor(EfficientNetV2B0, 'top_dropout'), query_path, features_nn, filenames, preprocess_input, output_number=image_limit)
+    intersection = [];
+    if features_handcrafted is not None:
+        handcrafted_most_similar, distances = find_similar_handcrafted(IMAGE_DIR, features_handcrafted, query_path, False, output_number=image_limit)
+        intersection = np.intersect1d(handcrafted_most_similar, nn_most_similar)
+
+    most_similar_filenames = nn_most_similar if use_nn else handcrafted_most_similar
+    most_similar_filenames = intersection if use_intersection else most_similar_filenames
+
+    most_similar_filenames = nn_most_similar
+    most_similar_filenames = [IMAGE_DIR + filename for filename in most_similar_filenames];
+
+    return most_similar_filenames;
