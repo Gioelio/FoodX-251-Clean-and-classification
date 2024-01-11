681b299395e871bb1a5982afb48cd6d584556b50
Gioelio <gioele.fiorenza2000@gmail.com>
2024-01-11 08:54:10 +0100

    added slider to control how base features should be used

--- gui/search.py
+++ gui/search.py
@@ -11,14 +11,24 @@ class SearchWindow(QtWidgets.QMainWindow):
         uic.loadUi('gui/similarity.ui', self)
         self.button_select = self.findChild(QtWidgets.QPushButton, 'buttonSelect')
         self.button_select.clicked.connect(self.select_clicked);
+        self.slider = self.findChild(QtWidgets.QSlider, "baseWeight")
+        self.slider.sliderReleased.connect(self.slider_released);
+        self.slider.setMinimum(1)
+        self.slider.setMaximum(9)
+        self.slider.setValue(5)
+        self.slider.setTickInterval(1)
         self.grid = self.findChild(QtWidgets.QScrollArea, 'gridImages');
-        self.query_img = self.findChild(QtWidgets.QLabel, 'queryImg')
+        self.query_img = self.findChild(QtWidgets.QLabel, 'queryImg');
 
         nn, filenames = load_images_for_gui()
         self.nn_features = nn;
         self.filenames = filenames;
         self.handcrafted = None
+        self.filename = None;
 
+    def slider_released(self):
+        if self.filename is not None:
+            self.load_images_in_grid(self.filename)
 
     def select_clicked(self):
         dialog = QFileDialog()
@@ -26,39 +36,42 @@ class SearchWindow(QtWidgets.QMainWindow):
         dialog.setNameFilter("Immagini (*.jpg *.png *.jpeg *.bmp);; Tutti i file (*.*)")
         if dialog.exec():
             filename = dialog.selectedFiles()[0]
-            print(filename)
-            pixmap = QPixmap(filename)
-            self.query_img.setPixmap(pixmap.scaledToWidth(200))
-            self.query_img.setAlignment(Qt.AlignmentFlag.AlignCenter)
+            self.load_images_in_grid(filename)
+            
 
-            use_intersection = False,
-            use_nn = True
+    def load_images_in_grid(self, filename):
+        print(filename)
+        pixmap = QPixmap(filename)
+        self.query_img.setPixmap(pixmap.scaledToWidth(200))
+        self.query_img.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-            most_similar_filenames = find_images_from_gui(filename, self.handcrafted, self.nn_features, self.filenames, use_intersection, use_nn)
-            row, col = 0, 0
-            number_limit = 100
+        use_intersection = False,
+        use_nn = True
 
-            scrollAreaWidget = QWidget()
-            grid_layout = QGridLayout(scrollAreaWidget)
-            for path in most_similar_filenames:
-                if number_limit <= 0:
-                    continue;
-                number_limit -= 1;
-                pixmap = QPixmap(path)
-                label = QLabel(self)
-                label.setPixmap(pixmap.scaledToWidth(200))
-                label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        most_similar_filenames = find_images_from_gui(filename, self.handcrafted, self.nn_features, self.filenames, use_intersection, use_nn, self.slider.value()/10)
+        row, col = 0, 0
+        number_limit = 100
 
-                grid_layout.addWidget(label, row, col)
-                col += 1;
+        scrollAreaWidget = QWidget()
+        grid_layout = QGridLayout(scrollAreaWidget)
+        for path in most_similar_filenames:
+            if number_limit <= 0:
+                continue;
+            number_limit -= 1;
+            pixmap = QPixmap(path)
+            label = QLabel(self)
+            label.setPixmap(pixmap.scaledToWidth(200))
+            label.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-                if col == 2:
-                    col = 0;
-                    row += 1
+            grid_layout.addWidget(label, row, col)
+            col += 1;
 
-            widget = QWidget()
-            self.grid.setWidget(scrollAreaWidget);
+            if col == 2:
+                col = 0;
+                row += 1
 
-            self.setGeometry(100, 100, 800, 600)
+        self.filename = filename
+        self.grid.setWidget(scrollAreaWidget);
 
+        self.setGeometry(100, 100, 800, 600)
             
--- similarity_search/similarity_search_gui.py
+++ similarity_search/similarity_search_gui.py
@@ -4,20 +4,25 @@ import sys
 import os
 import numpy as np
 from tensorflow import keras
+from sklearn.preprocessing import normalize
 
 sys.path.append('..')
 
 from keras.applications.efficientnet_v2 import EfficientNetV2B0, preprocess_input
 from similarity_search.neural_similarity_search import find_similar, find_similar_handcrafted
 from handcrafted_extraction import load_all_features
+from feature_extraction import build_feature_extractor 
 from vit_keras import vit
 
 HANDCRAFTED_FEATURES = 'dataset/handcrafted/'
-EN_FEATURES = 'similarity_search/extracted_features/efficient_net_extended_similarity_test'
-EN_FILENAMES = 'similarity_search/extracted_features/efficient_net_extended_similarity_test_filenames.csv'
+EN_FEATURES = 'similarity_search/extracted_features/efficient_net_extended_similarity'
+EN_FILENAMES = 'similarity_search/extracted_features/efficient_net_extended_similarity_filenames.csv'
 
-VIT_FEATURES = 'similarity_search/extracted_features/vitb16_extended_similarity_test'
-VIT_FILENAMES = 'similarity_search/extracted_features/vitb16_extended_similarity_test_filenames.csv'
+BASE_FEATURES = 'similarity_search/extracted_features/efficient_net_not_tuned_similarity'
+BASE_FILENAMES = 'similarity_search/extracted_features/efficient_net_not_tuned_similarity_filenames.csv'
+
+VIT_FEATURES = 'similarity_search/extracted_features/vitb16_extended_similarity'
+VIT_FILENAMES = 'similarity_search/extracted_features/vitb16_extended_similarity_filenames.csv'
 
 IMAGE_DIR = 'dataset/complete/'
 
@@ -27,44 +32,62 @@ VIT_FEATURE_EXTRACTOR = keras.Sequential(VIT_FEATURE_EXTRACTOR.layers[:-1])
 EN_FEATURE_EXTRACTOR = keras.models.load_model('classification/tuned_models/efficientnet_v2_noise_extended')
 EN_FEATURE_EXTRACTOR = keras.Sequential(EN_FEATURE_EXTRACTOR.layers[:-1])
 
+BASE_FEATURES_EXTRACTOR = build_feature_extractor(EfficientNetV2B0, 'block6h_se_reduce')
 
 
 def load_images_for_gui(type='nn'):
     if type == 'nn':
         vit_features = np.load(VIT_FEATURES + '.npy')
         en_features = np.load(EN_FEATURES + '.npy')
+        base_features = np.load(BASE_FEATURES + '.npy')
     else:
         features, _ = load_all_features(HANDCRAFTED_FEATURES, os.listdir(IMAGE_DIR), load_color=True, load_lbp=True,
                                         load_gabor=False, load_sift=False, load_bow=False)
 
     filenames = pd.read_csv(VIT_FILENAMES, header=None).iloc[:, 1].values
 
-    return (vit_features, en_features), filenames
+    return (vit_features, en_features, base_features), filenames
 
 
-def find_images_from_gui(query_path, features_handcrafted, features_nn, filenames, use_intersection, use_nn):
-    from feature_extraction import build_feature_extractor
+def find_images_from_gui(query_path, features_handcrafted, features_nn, filenames, use_intersection, use_nn, base_weight=0.5):
     img = cv.imread(query_path)
     img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+
+    print('base_weight: ', base_weight)
 
     image_limit = 10000
     vit_most_similar, vit_distances = find_similar(VIT_FEATURE_EXTRACTOR, query_path, features_nn[0], filenames,
                                                    vit.preprocess_inputs, output_number=image_limit)
     en_most_similar, en_distances = find_similar(EN_FEATURE_EXTRACTOR, query_path, features_nn[1], filenames,
                                                    preprocess_input, output_number=image_limit)
+    
+    base_filenames = pd.read_csv(BASE_FILENAMES, header=None).iloc[:, 1].values
+
+    base_most_similar, base_distances = find_similar(BASE_FEATURES_EXTRACTOR, query_path, features_nn[2], base_filenames,
+                                                     preprocess_input, output_number=image_limit)
 
     intersection = np.intersect1d(vit_most_similar, en_most_similar)
+    #intersection = np.intersect1d(intersection, base_most_similar)
+
+    vit_distances = vit_distances / np.linalg.norm(vit_distances)
+    en_distances = en_distances / np.linalg.norm(en_distances)
+    base_distances = base_distances / np.linalg.norm(base_distances)
+
     sums = {}
     for el in intersection:
         i = np.where(vit_most_similar == el)
-        sums[el] = vit_distances[i]
+        sums[el] = vit_distances[i] * (1 - base_weight) / 2
         j = np.where(en_most_similar == el)
-        sums[el] += en_distances[j]
-
-    print(sums)
+        sums[el] += en_distances[j] * (1 - base_weight) / 2
+    for el in base_most_similar:
+        k = np.where(base_most_similar == el)
+        value = base_distances[k] * base_weight;
+        if el in sums.keys():
+            sums[el] += value
+        else:
+            sums[el] = value
 
     nn_most_similar = {k: v for k, v in sorted(sums.items(), key=lambda item: item[1])}
-    print(nn_most_similar)
 
     # i = 0
     # j = 0
