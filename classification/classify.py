aff4800397f86f7f014d1e769fc5d5a1a4d80eeb
Gabriele Canesi <gabrielecanesi999@gmail.com>
2024-01-14 18:55:39 +0100

    Updated ensembling with geometric mean and preprocessing

--- classification/classify.py
+++ classification/classify.py
@@ -1,9 +1,7 @@
 import numpy as np
-from tensorflow import keras
 import cv2 as cv
 from vit_keras import vit
 from keras.applications.efficientnet_v2 import preprocess_input
-from scipy.stats import entropy
 from misc import load_class_labels, import_model
 
 
@@ -21,15 +19,10 @@ def classify_image(path):
     image = np.expand_dims(image, 0)
 
     vit_prediction = VIT_MODEL.predict(vit.preprocess_inputs(image))[0]
-    efficientnet_prediction = EN_MODEL.predict(preprocess_input(image))[0]
+    en_prediction = EN_MODEL.predict(preprocess_input(image))[0]
 
-    vit_entropy = 1 / entropy(vit_prediction)
-    en_entropy = 1 / entropy(efficientnet_prediction)
-
-    normalized_vit_entropy = vit_entropy / (vit_entropy + en_entropy)
-    normalized_en_entropy = en_entropy / (en_entropy + vit_entropy)
-
-    ensemble_prediction = normalized_vit_entropy * vit_prediction + normalized_en_entropy * efficientnet_prediction
+    ensemble_prediction = np.sqrt(vit_prediction * en_prediction)
+    ensemble_prediction /= ensemble_prediction.sum()
     ordered_classes = np.argsort(-ensemble_prediction)
 
     return ordered_classes, ensemble_prediction, classnames
--- preprocessing.py
+++ preprocessing.py
@@ -3,36 +3,31 @@ import numpy as np
 
 
 def noise_removal(image):
-    image = cv.cvtColor(image, cv.COLOR_BGR2YCrCb)
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    gradient = cv.Laplacian(gray, ddepth=3)
+    mean, std = cv.meanStdDev(gradient)
 
-    ksize = 15
+    if std[0, 0] > 100:
+        image = cv.cvtColor(image, cv.COLOR_BGR2YCrCb)
+        ksize = 15
 
-    image[:, :, 1] = cv.GaussianBlur(image[:, :, 1], (ksize, ksize), 9, None, 9)
-    image[:, :, 2] = cv.GaussianBlur(image[:, :, 2], (ksize, ksize), 9, None, 9)
+        image[:, :, 1] = cv.GaussianBlur(image[:, :, 1], (ksize, ksize), 9, None, 9)
+        image[:, :, 2] = cv.GaussianBlur(image[:, :, 2], (ksize, ksize), 9, None, 9)
 
-    image = cv.cvtColor(image, cv.COLOR_YCrCb2BGR)
-    image = cv.bilateralFilter(image, 11, 150, 15)
-    image = cv.GaussianBlur(image, (3, 3), 5, None, 5)
+        image = cv.cvtColor(image, cv.COLOR_YCrCb2BGR)
+        image = cv.GaussianBlur(image, (3, 3), 5, None, 5)
+        image = cv.bilateralFilter(image, 7, 140, 5)
     return image
 
 
 def white_balance(image, threshold1=0.5, threshold2=0.5):
-    gray = cv.cvtColor(image, cv.COLOR_BGR2YCrCb)[:, :, 0]
     sat = (np.max(image, axis=2) - np.min(image, axis=2)) / np.max(image, axis=2)
-    sat[np.isnan(sat)] = 0
 
-    if sat.mean() > 0.5:
-        sat_threshold = np.quantile(sat.flatten(), threshold1)
-        wb = cv.xphoto.createSimpleWB()
+    sat_threshold = np.quantile(sat[~np.isnan(sat)].flatten(), threshold2)
 
-        wb.setP(sat_threshold)
-        image = wb.balanceWhite(image)
-
-    else:
-        sat_threshold = np.quantile(sat.flatten(), (1 - threshold2))
-        wb = cv.xphoto.createGrayworldWB()
-        wb.setSaturationThreshold(sat_threshold)
-        image = wb.balanceWhite(image)
+    wb = cv.xphoto.createGrayworldWB()
+    wb.setSaturationThreshold(sat_threshold)
+    image = wb.balanceWhite(image)
 
     return image
 
@@ -46,8 +41,6 @@ def local_gamma_correction(image):
     image[:, :, 0] = y_channel
 
     image = cv.cvtColor(image, cv.COLOR_YCrCb2BGR)
-    # cv.imshow('mask', mask)
-    # cv.waitKey(0)
     return image
 
 
@@ -70,56 +63,43 @@ def contrast_stretch(image, ignore_p=0, min_val=0, max_val=255):
     return image
 
 
-def gamma_correction(image, type):
-    y = cv.cvtColor(image, cv.COLOR_BGR2YCrCb)
-    q1 = np.quantile(y[:, :, 0], 0.5)
-    if type == 'low':
-        print(q1)
-        y[:, :, 0] = (y[:, :, 0] / 255.) ** 0.7 * 255
-    else:
-        y[:, :, 0] = (y[:, :, 0] / 255.) ** 1.3 * 255
+def gamma_correction(image):
+    image = cv.cvtColor(image, cv.COLOR_BGR2YCrCb)
+    y_channel = image[:, :, 0]
+    y_channel = 255. * (y_channel / 255.) ** (1.3 ** (y_channel.mean() / 128.))
+    image[:, :, 0] = y_channel
 
-    image = cv.cvtColor(y, cv.COLOR_YCrCb2BGR)
+    image = cv.cvtColor(image, cv.COLOR_YCrCb2BGR)
     return image
 
 
-def pipeline(image, wb_threshold=0.5, wb_threshold2=0.5):
+def compute_low_high_prop(image):
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    gradient = cv.Laplacian(gray, ddepth=3)
-    mean, std = cv.meanStdDev(gradient)
-    image = white_balance(image, wb_threshold, wb_threshold2)
-
     blurred = cv.GaussianBlur(gray, (7, 7), 15, None, 15)
-    count_low = blurred[blurred < 75].flatten().shape[-1]
-    count_high = blurred[blurred > 180].flatten().shape[-1]
+    count_low = blurred[blurred < 60].flatten().shape[-1]
+    count_high = blurred[blurred > 195].flatten().shape[-1]
 
     total_black_count = blurred[blurred <= 5].flatten().shape[-1]
     total_white_count = blurred[blurred >= 250].flatten().shape[-1]
 
     low_prop = (count_low - total_black_count) / (blurred.flatten().shape[-1] - total_black_count)
     high_prop = (count_high - total_white_count) / (blurred.flatten().shape[-1] - total_white_count)
+    return low_prop, high_prop
 
-    if low_prop + high_prop > 0.7 and (low_prop > 0.5 and high_prop > 0.2):
-        # cv.destroyAllWindows()
-        # cv.imshow('before', image)
-        # cv.waitKey(1)
+
+def pipeline(image, wb_threshold=0.5, wb_threshold2=0.5):
+    image = noise_removal(image)
+
+    low_prop, high_prop = compute_low_high_prop(image)
+
+    if (low_prop > 0.5 and high_prop < 0.1) or (low_prop < 0.1 and high_prop > 0.6):
+        image = gamma_correction(image)
+        low_prop, high_prop = compute_low_high_prop(image)
+
+    if low_prop + high_prop > 0.6 and (low_prop > 0.4 and high_prop > 0.15):
         image = local_gamma_correction(image)
-        image = contrast_stretch(image, 2)
-        # cv.imshow(str(low_prop) + " " + str(high_prop), image)
-        # cv.waitKey(0)
+        image = contrast_stretch(image)
 
-    elif low_prop > 0.7 or high_prop > 0.7:
-        # cv.destroyAllWindows()
-        # cv.imshow('before', image)
-        # cv.waitKey(1)
-        image = gamma_correction(image, 'low' if low_prop > 0.7 else 'high')
-        # cv.imshow(str(low_prop) + " " + str(high_prop), image)
-        # cv.waitKey(0)
-        pass
-
-    #
-    if std[0, 0] > 100:
-        image = noise_removal(image)
-        # contrast_stretch(image, 5)
+    image = white_balance(image, wb_threshold, wb_threshold2)
 
     return image
