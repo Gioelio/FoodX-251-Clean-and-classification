520d8baeba21a8061afcc5d0f022322eb58f3d3b
Gabriele Canesi <gabrielecanesi999@gmail.com>
2024-01-15 11:35:09 +0100

    Updated writing of preprocessed images

--- restoration/image_quality.py
+++ restoration/image_quality.py
@@ -10,8 +10,9 @@ def build_restored_dataset(source_path, destination_path):
     os.makedirs(destination_path, exist_ok=True)
     q = brisque.BRISQUE(url=False)
     for file in tqdm(glob.glob(source_path + '*.jpg')):
-        image = cv.imread(file)
-        image = pipeline(image, 0.02, 0.05)
-        # quality = q.score(image)
-        # if quality < 85:
-        cv.imwrite(destination_path + os.path.basename(file), image)
+        if not os.path.exists(destination_path + os.path.basename(file)):
+            image = cv.imread(file)
+            image = pipeline(image, 0.02, 0.05)
+            quality = q.score(image)
+            if quality < 85:
+                cv.imwrite(destination_path + os.path.basename(file), image)
